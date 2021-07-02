27250a4e0a54a3149f8ee15bf0cb17d21f80b118
Daniel Cunha <cunhacdaniel@gmail.com>
2021-07-02 18:58:30 -0400

    add validations

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.devsuperior.dslearnbds.resources;
+
+
+import com.devsuperior.dslearnbds.dto.UserDTO;
+import com.devsuperior.dslearnbds.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+        UserDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/exceptions/DataBaseException.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dslearnbds.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+}
